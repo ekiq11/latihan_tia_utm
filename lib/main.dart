24667ece3af9635ead064334576b319f76619e7d
subki1992 <ahmad.subki1992@utmmataram.ac.id>
2024-10-15 02:43:24 +0000

    Tes

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,32 @@
+//Ini wajib
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//ini kode yang akan pertama kali dijalankan oleh flutter
+void main() {
+  //MyApp ini adalah kelas
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: MyHome());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Colors.green,
+        title: Text("Whatsapp"),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      body: Center(child: Text("Hello World")),
     );
   }
 }
